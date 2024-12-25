c8c11317667bda5cec3598166196406470417ecd
Arthur Nasser <arthurnasser@gmail.com>
2024-12-25 16:03:03 -0300

    icone git

--- src/components/footer/SocialLinks.tsx
+++ src/components/footer/SocialLinks.tsx
@@ -1,4 +1,5 @@
-import {  Instagram, Linkedin } from 'lucide-react';
+import { Instagram, Linkedin } from 'lucide-react';
+import { FaGithub } from 'react-icons/fa';
 
 export default function SocialLinks() {
   return (
@@ -22,7 +23,15 @@ export default function SocialLinks() {
       >
         <Linkedin className="w-5 h-5" />
       </a>
-
+      <a
+        href="https://github.com/arthurnasser"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="h-6 w-6 hover:text-gray-400"
+        aria-label="Github"
+      >
+        <FaGithub className="w-5 h-5" />
+        </a>
     </div>
   );
 }
